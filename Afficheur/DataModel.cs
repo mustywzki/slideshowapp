9e9c4e9b3e4c39e7744e0a8dbfb68bc1154a4bac
Cédric Jolly <cedric.jolly@etu.u-bordeaux.fr>
2017-05-26 18:45:31 +0200

    first commit

---
+++ Afficheur/DataModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Exif;
+
+namespace Afficheur
+{
+    class DataModel
+    {
+        public List<JPGFileData> Fichiers { get; set; }
+
+        public DataModel(string chemin)
+        {
+            Fichiers = new List<JPGFileData>();
+            string[] fichiers = Directory.GetFiles(chemin);
+            fichiers.ToList().Where(c => c.ToLower().EndsWith("jpg"))   //Création de la liste de fichier en jpg
+                .ToList().ForEach(c => Fichiers.Add(new JPGFileData(c)));  //Ajout de cette liste dans Fichiers
+        }
+
+        public DataModel(List<JPGFileData> files)
+        {
+            Fichiers = new List<JPGFileData>();
+            Fichiers = files;
+        }
+
+        private List<JPGFileData> getListFileData()
+        {
+            return Fichiers;
+        }
+    }
+}
---
+++ Afficheur/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Exif;
+
+namespace Afficheur
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private string path { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            path = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            DataContext = new DataModel(path);
+        }
+
+        private List<JPGFileData> stringToList()
+        {
+            List<JPGFileData> temp = new List<JPGFileData>();
+            string[] fichiers = Directory.GetFiles(path);
+            fichiers.ToList().Where(c => c.ToLower().EndsWith("jpg"))   //Création de la liste de fichier en jpg
+                .ToList().ForEach(c => temp.Add(new JPGFileData(c)));  //Ajout de cette liste dans Fichiers
+
+            return temp;
+        }
+
+        private void Diaporama_Click(object sender, RoutedEventArgs e)
+        {
+            DiapoWindow diapo = new DiapoWindow(this.stringToList());
+            diapo.Show();
+        }
+
+        private void EditItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
